6a1c52fea8622cee250e5e30893058ab14bf354e
Michał Ambroży <michal@ambrozy.pl>
2021-08-13 23:14:11 +0200

    Add snap helper

--- ui/src/main/java/com/ambrozy/ui/bindings/RecyclerViewBindings.kt
+++ ui/src/main/java/com/ambrozy/ui/bindings/RecyclerViewBindings.kt
@@ -2,6 +2,7 @@ package com.ambrozy.ui.bindings
 
 import android.widget.LinearLayout
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.ambrozy.ui.views.ItemDecorator
 
@@ -24,5 +25,12 @@ class RecyclerViewBindings {
         verticalMargin
       )
     )
+  }
+
+  @BindingAdapter("attachSnapHelper")
+  fun RecyclerView.bindSnapHelper(attach: Boolean) {
+    if (attach) {
+      LinearSnapHelper().attachToRecyclerView(this)
+    }
   }
 }
